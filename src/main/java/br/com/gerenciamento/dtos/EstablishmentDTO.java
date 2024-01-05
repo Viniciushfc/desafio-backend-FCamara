f22d70b338dc397e739d8f430fb802c1d4667bcd
ViniciusHFC <vinicius.henry.fc@gmail.com>
2024-01-05 18:08:50 -0300

    Criação de EstablishmentDTO.

---
+++ src/main/java/br/com/gerenciamento/dtos/EstablishmentDTO.java
@@ -0,0 +1,4 @@
+package br.com.gerenciamento.dtos;
+
+public record EstablishmentDTO(String name, String document, String address, String telephone, String amountVacanciesBike, String amountVacanciesCar) {
+}
