065547fdd15bfb92dff9498cd6ff95ebd883d651
ViniciusHFC <vinicius.henry.fc@gmail.com>
2024-01-16 15:44:37 -0300

    Correção do nome do método.

--- src/main/java/br/com/gerenciamento/controllers/EstablishmentController.java
+++ src/main/java/br/com/gerenciamento/controllers/EstablishmentController.java
@@ -43,7 +43,7 @@ public class EstablishmentController {
     }
 
     @PutMapping
-    public ResponseEntity<Establishment> updateEstablishments(@RequestBody EstablishmentDTO dto) {
+    public ResponseEntity<Establishment> updateEstablishment(@RequestBody EstablishmentDTO dto) {
         Establishment establishment = establishmentService.updateEstablishment(dto);
         return new ResponseEntity<>(establishment, HttpStatus.OK);
     }
