7c8447b32ec2fed1290b8adb26bd10f32d86ec2c
ViniciusHFC <vinicius.henry.fc@gmail.com>
2024-01-17 17:22:46 -0300

    Adicionado Repository para o controle de veículos.

---
+++ src/main/java/br/com/gerenciamento/repositories/ExitEntryControlRepository.java
@@ -0,0 +1,7 @@
+package br.com.gerenciamento.repositories;
+
+import br.com.gerenciamento.domain.exitEntryControl.ExitEntryControl;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExitEntryControlRepository extends JpaRepository<ExitEntryControl, Long> {
+}
