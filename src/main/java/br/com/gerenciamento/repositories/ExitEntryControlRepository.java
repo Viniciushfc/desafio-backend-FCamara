b89a30be754ad49c8285dee7a4b4feeff50b7a0b
ViniciusHFC <vinicius.henry.fc@gmail.com>
2024-01-18 20:06:50 -0300

    Adicionado metodos para JPA findExitEntryControlById e findExitEntryControlByDocument.

--- src/main/java/br/com/gerenciamento/repositories/ExitEntryControlRepository.java
+++ src/main/java/br/com/gerenciamento/repositories/ExitEntryControlRepository.java
@@ -1,7 +1,14 @@
 package br.com.gerenciamento.repositories;
 
+import br.com.gerenciamento.domain.establishment.Establishment;
 import br.com.gerenciamento.domain.exitEntryControl.ExitEntryControl;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ExitEntryControlRepository extends JpaRepository<ExitEntryControl, Long> {
+
+    Optional<ExitEntryControl> findExitEntryControlById(Long id);
+
+    Optional<ExitEntryControl> findExitEntryControlByDocument(String document);
 }
