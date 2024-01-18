51836603b0a8d51e1dbcc7bda73975a6c43b73b4
ViniciusHFC <vinicius.henry.fc@gmail.com>
2024-01-18 20:05:30 -0300

    Adicionado endpoint de GetAllExitEntryControl e updateExitEntryControl.

--- src/main/java/br/com/gerenciamento/controllers/ExitEntryControlController.java
+++ src/main/java/br/com/gerenciamento/controllers/ExitEntryControlController.java
@@ -6,10 +6,9 @@ import br.com.gerenciamento.services.ExitEntryControlService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/control")
@@ -21,6 +20,18 @@ public class ExitEntryControlController {
     @PostMapping
     public ResponseEntity<ExitEntryControl> createExitEntryControl(@RequestBody ExitEntryControlDTO dto){
         ExitEntryControl exitEntryControl = exitEntryControlService.createExitEntryControl(dto);
+        return new ResponseEntity<>(exitEntryControl, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ExitEntryControl>> getAllExitEntryControl(){
+        List<ExitEntryControl> exitEntryControls = exitEntryControlService.getAllExitEntryControl();
+        return new ResponseEntity<>(exitEntryControls, HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<ExitEntryControl> updateExitEntryControl(@RequestBody ExitEntryControlDTO dto){
+        ExitEntryControl exitEntryControl = exitEntryControlService.updateExitEntryControl(dto);
         return new ResponseEntity<>(exitEntryControl, HttpStatus.OK);
     }
 }
