0e30f37ff2c4f901b656a9872f3084780be9ca23
ViniciusHFC <vinicius.henry.fc@gmail.com>
2024-01-18 20:06:15 -0300

    retirado entry e exit da DTO utilizao inútil.

--- src/main/java/br/com/gerenciamento/dtos/ExitEntryControlDTO.java
+++ src/main/java/br/com/gerenciamento/dtos/ExitEntryControlDTO.java
@@ -1,6 +1,5 @@
 package br.com.gerenciamento.dtos;
 
-import java.time.LocalDateTime;
 
-public record ExitEntryControlDTO(LocalDateTime entry, LocalDateTime exit, String plate, String document) {
+public record ExitEntryControlDTO(String plate, String document) {
 }
