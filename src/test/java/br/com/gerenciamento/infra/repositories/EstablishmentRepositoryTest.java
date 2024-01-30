454fce0fcbc7c50744a4341366a25ca50dd2c86f
ViniciusHFC <vinicius.henry.fc@gmail.com>
2024-01-30 17:23:33 -0300

    Começo dos Testes Unitários, Establishment Repository Test, fins didático para entender e relembrar como funciona os Tests.

---
+++ src/test/java/br/com/gerenciamento/infra/repositories/EstablishmentRepositoryTest.java
@@ -0,0 +1,52 @@
+package br.com.gerenciamento.infra.repositories;
+
+import br.com.gerenciamento.domain.establishment.Establishment;
+import br.com.gerenciamento.dtos.EstablishmentDTO;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@DataJpaTest
+@ActiveProfiles("test")
+class EstablishmentRepositoryTest {
+
+    @Autowired
+    EstablishmentRepository establishmentRepository;
+
+    @Autowired
+    EntityManager entityManager;
+
+    @Test
+    @DisplayName("Should get Establishment successfully from DB")
+    void findEstablishmentByDocumentCase1() {
+        String document = "123456781";
+        EstablishmentDTO dto = new EstablishmentDTO("Estabelecimento", document, "Ali mesmo", "(99)99999-9998", 10L, 10L);
+        this.createEstablishment(dto);
+        Optional<Establishment> result = this.establishmentRepository.findEstablishmentByDocument(document);
+
+        assertThat(result.isPresent()).isTrue();
+    }
+
+    @Test
+    @DisplayName("Should not get Establishment from DB when establishment not exists")
+    void findEstablishmentByDocumentCase2() {
+        String document = "123456781";
+        Optional<Establishment> result = this.establishmentRepository.findEstablishmentByDocument(document);
+
+        assertThat(result.isEmpty()).isTrue();
+    }
+
+    private Establishment createEstablishment(EstablishmentDTO dto){
+        Establishment establishment = new Establishment(dto);
+        this.entityManager.persist(establishment);
+        return establishment;
+    }
+}
